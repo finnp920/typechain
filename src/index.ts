3e8d90841b291982f4382dcc0dfb21c7f9916725
finnp920 <finnp920@gmail.com>
2022-03-31 19:16:30 +0900

    Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
-interface Human {
-  name: string,
-  age: number,
-  gender: string
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  getAge() {
+    return this.age;
+  }
 }
 
-const preson = {
-  name: "finn",
-  age: 33,
-  gender: "female"
-}
+const finn = new Human("Finn", 33, "female")
 
 const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+  return `Hello ${person.name}, you are ${person.getAge()}, you are a ${person.gender}!`;
 }
 
-console.log(sayHi(preson));
+console.log(sayHi(finn));
 
 export {};
