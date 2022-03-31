52c964b989183abcef7c6ca89e729275578134d9
finnp920 <finnp920@gmail.com>
2022-03-31 19:05:13 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender:string): void => {
-  console.log(`Hello ${name}, you are ${age}, you are a ${gender}!`);
+interface Human {
+  name: string,
+  age: number,
+  gender: string
 }
 
-sayHi("finn", 33, "female");
+const preson = {
+  name: "finn",
+  age: 33,
+  gender: "female"
+}
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+}
+
+console.log(sayHi(preson));
 
 export {};
