a3feba656e50fd2beb8594de3bba3e0ed7b02ab1
finnp920 <finnp920@gmail.com>
2022-05-15 19:40:26 +0900

    5.2 Lib Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return 'hi';
-  }
-}
+// tsconfig > lib에 "DOM"을 설정해서, 브라우저 API의 자동완성 기능을 사용할 수 있게됨
+document.querySelector('');
+localStorage.setItem('', '');
