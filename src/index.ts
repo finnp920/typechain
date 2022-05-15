e240c53e7bcfcd39a79ecc6b76c3af6356020ffd
finnp920 <finnp920@gmail.com>
2022-05-15 21:03:27 +0900

    5.3 Declaration Files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-// tsconfig > lib에 "DOM"을 설정해서, 브라우저 API의 자동완성 기능을 사용할 수 있게됨
-document.querySelector('');
-localStorage.setItem('', '');
+import { init, exit } from "myPackage";
+init({
+  url: "true",
+});
+exit(1);
